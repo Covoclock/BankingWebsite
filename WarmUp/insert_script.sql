efc5983d62e4f4889783a223d366b4e548b7e4b1
rickytechnip <35615134+rickytechnip@users.noreply.github.com>
2018-09-29 15:37:12 -0400

    Populate Client table

--- WarmUp/insert_script.sql
+++ WarmUp/insert_script.sql
@@ -49,3 +49,17 @@ VALUES(2021, 'Jack',  'Atheia', '111  frank', '2005-11-29', 63324.61, 'mnbv@gmai
 INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id, postion_id)
 VALUES(2022, 'Kim',  'Stefen', '453  Demo', '2005-11-29', 63333.22, 'zxcvds@gmail.com', '450-455-3333', 202,11);
 -- vim:et
+
+-- ------------------------
+-- Populate Client table
+-- ------------------------
+
+INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
+VALUE(100001,'Ricky', 'Martin', '342 rue berlioz', '1980-02-22', '2003-1-12', 'bnos@gmail.com', '514-222-3456', 100);
+INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
+VALUE(101002,'Mei', 'Yuan', '111 rue Young', '1990-03-11', '2003-12-24', 'trdhge@gmail.com', '514-222-6544', 101);
+INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
+VALUE(201001,'Tina', 'Ita', '675 rue William', '1983-02-22', '2004-12-26', 'bnos@gmail.com', '450-222-9877', 201);
+INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
+VALUE(202002,'Bob', 'Bush', '456 rue Young', '1995-03-11', '2005-12-28', 'trdhge@gmail.com', '450-222-5675', 202);
+
