cff0853b38bf2a983bb6a5fcb3093da25be4feb6
Sebastien <sebastien.bah@gmail.com>
2018-09-30 16:58:02 -0400

    Removed the employee_id attr in the inset statement as it was not specified

--- WarmUp/insert_script.sql
+++ WarmUp/insert_script.sql
@@ -30,28 +30,28 @@ VALUES('Quebec', 'Montreal', 'Cote Des Neiges', '514-555-1515', '514-555-5151', 
 -- Populate Employee table
 -- ------------------------
 
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'John',  'Smith', '219 Rue Berlioz', '2001-02-22', 56188.12, 'awoeif@gmail.com', '514-345-3349', 1);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Michelle',  'Green', '233 Rue Gika', '2001-02-22', 58188.12, 'regtyj@gmail.com', '514-455-8988', 1);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Jim',  'Jones', '111 Rue Rigo', '2003-05-20', 65444.11, 'tyjdrheg@gmail.com', '514-455-6576', 2);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Kim',  'Howard', '453 Rue Tika', '2003-05-20', 76666.21, 'jyrthe@gmail.com', '514-455-3333', 2);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Ann',  'Roger', '219  Trudeau', '2004-08-21', 59321.34, 'jythrt@gmail.com', '450-345-3349', 3);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Tim',  'King', '233  Tilan', '2004-08-21', 61222.11, 'iuwsd@gmail.com', '450-455-8988', 3);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Jack',  'Atheia', '111  frank', '2005-11-29', 63324.61, 'mnbv@gmail.com', '450-455-6576', 4);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Kim',  'Stefen', '453  Demo', '2005-11-29', 63333.22, 'zxcvds@gmail.com', '450-455-3333', 4);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'John',  'Smith', '219 Rue Berlioz', '2001-02-22', 56188.12, 'awoeif@gmail.com', '514-345-3349', 1);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Michelle',  'Green', '233 Rue Gika', '2001-02-22', 58188.12, 'regtyj@gmail.com', '514-455-8988', 1);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Jim',  'Jones', '111 Rue Rigo', '2003-05-20', 65444.11, 'tyjdrheg@gmail.com', '514-455-6576', 2);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Kim',  'Howard', '453 Rue Tika', '2003-05-20', 76666.21, 'jyrthe@gmail.com', '514-455-3333', 2);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Ann',  'Roger', '219  Trudeau', '2004-08-21', 59321.34, 'jythrt@gmail.com', '450-345-3349', 3);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Tim',  'King', '233  Tilan', '2004-08-21', 61222.11, 'iuwsd@gmail.com', '450-455-8988', 3);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Jack',  'Atheia', '111  frank', '2005-11-29', 63324.61, 'mnbv@gmail.com', '450-455-6576', 4);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Kim',  'Stefen', '453  Demo', '2005-11-29', 63333.22, 'zxcvds@gmail.com', '450-455-3333', 4);
 
 /* Cote Des Neiges */
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Kilian',  'karl', '453  Demo', '2003-11-29', 63333.22, 'zsadas@gmail.com', '450-555-5533', 4);
-INSERT INTO Employee(employee_id, firstName, lastName, addr, start_date, salary, email, phone, branch_id)
-VALUE( 'Karlito',  'kilimanjaro', '453  Demo', '2003-11-29', 63333.22, 'as3ds@gmail.com', '450-555-3355', 4);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Kilian',  'karl', '453  Demo', '2003-11-29', 63333.22, 'zsadas@gmail.com', '450-555-5533', 4);
+INSERT INTO Employee(firstName, lastName, addr, start_date, salary, email, phone, branch_id)
+VALUES( 'Karlito',  'kilimanjaro', '453  Demo', '2003-11-29', 63333.22, 'as3ds@gmail.com', '450-555-3355', 4);
 
 
 -- ------------------------
@@ -60,57 +60,57 @@ VALUE( 'Karlito',  'kilimanjaro', '453  Demo', '2003-11-29', 63333.22, 'as3ds@gm
 
 
 INSERT INTO Client(firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('Ricky', 'Martin', '342 rue Cote Des Neiges', '1980-02-22', '2003-1-12', 'bnos@gmail.com', '514-222-3456', 1);
+VALUES('Ricky', 'Martin', '342 rue Cote Des Neiges', '1980-02-22', '2003-1-12', 'bnos@gmail.com', '514-222-3456', 1);
 INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('Roberto', 'Yuan', '111 rue Young', '1990-03-11', '2003-12-24', 'trdhge@gmail.com', '514-222-6544', 2);
+VALUES('Roberto', 'Yuan', '111 rue Young', '1990-03-11', '2003-12-24', 'trdhge@gmail.com', '514-222-6544', 2);
 INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('Tina', 'Ita', '675 rue William', '1983-02-22', '2004-12-26', 'bnos@gmail.com', '450-222-9877', 3);
+VALUES('Tina', 'Ita', '675 rue William', '1983-02-22', '2004-12-26', 'bnos@gmail.com', '450-222-9877', 3);
 INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('Bob', 'Bush', '456 rue Young', '1995-03-11', '2005-12-28', 'trdhge@gmail.com', '450-222-5675', 4);
+VALUES('Bob', 'Bush', '456 rue Young', '1995-03-11', '2005-12-28', 'trdhge@gmail.com', '450-222-5675', 4);
 
 /* Cote Des Neiges */
 INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('Tina', 'turner', '675 rue ontario', '1983-02-22', '2003-10-16', 'bgsdos@gmail.com', '450-222-9877', 5);
+VALUES('Tina', 'turner', '675 rue ontario', '1983-02-22', '2003-10-16', 'bgsdos@gmail.com', '450-222-9877', 5);
 INSERT INTO Client(client_id, firstName, lastName, addr, dob, joining_date, email, phone,  branch_id)
-VALUE('billy', 'Bush', '456 rue sherbrooke', '1995-03-11', '2003-12-22', 'asdae@gmail.com', '450-222-5675', 5);
+VALUES('billy', 'Bush', '456 rue sherbrooke', '1995-03-11', '2003-12-22', 'asdae@gmail.com', '450-222-5675', 5);
 
 -- ------------------------
 -- Populate Account table
 -- ------------------------
 
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(1, 'saving', 'high interest', 8000.32,  9000.00, 0.02);
+VALUES(1, 'saving', 'high interest', 8000.32,  9000.00, 0.02);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(1, 'chequing', 'regular', 5432.32,  20000.00, 0.03);
+VALUES(1, 'chequing', 'regular', 5432.32,  20000.00, 0.03);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(2, 'saving', 'enhanced interest', 65432.11,  30000.00, 0.03);
+VALUES(2, 'saving', 'enhanced interest', 65432.11,  30000.00, 0.03);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(2, 'chequing', 'primium', 7655.23,  25000.00, 0.02);
+VALUES(2, 'chequing', 'primium', 7655.23,  25000.00, 0.02);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(3, 'saving', 'high interest', 20065.32,  25000.00, 0.04);
+VALUES(3, 'saving', 'high interest', 20065.32,  25000.00, 0.04);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(3, 'chequing', 'regular', 45333.32,  30000.00, 0.04);
+VALUES(3, 'chequing', 'regular', 45333.32,  30000.00, 0.04);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(4, 'saving', 'enhanced interest', 43544.11,  50000.00, 0.05);
+VALUES(4, 'saving', 'enhanced interest', 43544.11,  50000.00, 0.05);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(4, 'chequing', 'primium', 76554.23,  40000.00, 0.05);
+VALUES(4, 'chequing', 'primium', 76554.23,  40000.00, 0.05);
 
 /* Cote Des Neiges */
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(5, 'saving', 'enhanced interest', 435440.11,  50000.00, 0.08);
+VALUES(5, 'saving', 'enhanced interest', 435440.11,  50000.00, 0.08);
 INSERT INTO Account(client_id, account_type, account_option, balance, credit_limit, interest_rate)
-VALUE(6, 'chequing', 'regular', 7655.23,  40000.00, 0.05);
+VALUES(6, 'chequing', 'regular', 7655.23,  40000.00, 0.05);
 
 -- ------------------------
 -- Populate Services table
 -- ------------------------
 
 INSERT INTO Services(service_name, manager_id)
-VALUE('saving', 2);
+VALUES('saving', 2);
 INSERT INTO Services(service_name, manager_id)
-VALUE('chequing', 4);
+VALUES('chequing', 4);
 INSERT INTO Services(service_name, manager_id)
-VALUE('line of credit', 6);
+VALUES('line of credit', 6);
 INSERT INTO Services(service_name, manager_id)
-VALUE('loan', 8);
+VALUES('loan', 8);
 
