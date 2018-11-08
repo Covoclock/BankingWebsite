d4d398c6c3225ef15dadcb17b22913c53e2d3a4e
Yassin Bah <y_bah@turok.encs.concordia.ca>
2018-11-08 17:27:23 -0500

    Added tables (transactions and bills) and slight modifications to more accurately represent the ER diagram.

--- WarmUp/final_creation_script.sql
+++ WarmUp/final_creation_script.sql
@@ -73,6 +73,11 @@ CREATE TABLE IF NOT EXISTS ClientLogin (
         foreign key(client_id) References Client(client_id)
 );
 /* Account */
+/* 
+   Need an account for the bank so that
+   when charges/fees get processed they 
+   get sent to that account 
+*/
 CREATE TABLE IF NOT EXISTS Account (
         account_id      int not null auto_increment,
         client_id       int not null,f
@@ -83,6 +88,7 @@ CREATE TABLE IF NOT EXISTS Account (
         interest_rate   float,          -- positive and negative val 
         type            varchar(255),   -- checking, savings, credit 
         lvl             varchar(255),   -- personal, business or corporate
+        transactionLeft int, -- defaults to whats in charge plan
         foreign key(client_id) references Client(client_id),
         foreign key(chargePlan_id) references ChargePlan(chargePlan_id),
         primary key(account_id)
@@ -98,12 +104,41 @@ CREATE TABLE IF NOT EXISTS Services (
 );
 
 /* Charge plans */
-/*      */
 CREATE TABLE IF NOT EXISTS ChargePlan (
-        chargePlan_id       int not null,
+        chargePlan_id   int not null,
         option_name     varchar(255),
         draw_limit      float,
         charge_value    float, 
         primary key(chargePlan_id)
 );
+
+/* Transactions */
+CREATE TABLE IF NOT EXISTS Transactions (
+        tid             int not null auto_increment,
+        account1_id     int not null,
+        account2_id     int not null,
+        amount          decimal(16,2),
+        dt              datetime, -- 'YYYY-MM-DD HH:MM:SS' consider this to be atomic for our situation
+        primary key (tid),
+        foreign key(account1_id) references Account(account_id),
+        foreign key(account2_id) references Account(account_id)
+);
+
+/* Bills */
+/*
+  Table contains bills that have been setup and at the beginning
+  of each month the server will attempt to process all the bills.
+  It removes the ones that it could do and creates entries in the
+  transaction table. If it fails and it is recurrent then it duplicates
+  the bill (need to pay 2 bills next month to that account). If it
+  fails and not recurrent just leave it there for the next month.
+*/
+CREATE TABLE IF NOT EXISTS Bills(
+        bill_id         int not null auto_increment,
+        amount          decimal(16,2),
+        account1_id     int not null,
+        account2_id     int not null,
+        recurring       tinyint(1),
+        primary key(
+        
 -- vim:et
